f36bd2f3dbcf90db0b6394ff339e382d160ae7b1
ahmed khattab <akhattab@arabiclocalizer.com>
2023-10-04 20:50:45 +0300

    fix UTF8StringToArray function

--- src/Bidi.php
+++ src/Bidi.php
@@ -71,7 +71,7 @@ class Bidi
         $length = strlen($str);
         
         for ($i = 0; $i < $length; $i++) {
-            $char = ord($str{$i}); // get one string character at time
+            $char = ord($str[$i]); // get one string character at time
             if (count($bytes) == 0) { // get starting octect
                 if ($char <= 0x7F) {
                     $unicode[] = $char; // use the character "as is" because is ASCII
